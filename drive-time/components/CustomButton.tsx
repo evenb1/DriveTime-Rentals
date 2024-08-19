d0b07da33230ff19a93c647b9f54970ee6d56f2c
Even <even.berhane2022@gmail.com>
2024-08-19 22:06:23 +0300

    inclick function

--- drive-time/components/CustomButton.tsx
+++ drive-time/components/CustomButton.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from 'react';
 "use client";
+import { CustomButtonProps } from '@/types';
 
-const CustomButton = () => {
+const CustomButton = ({ title, containerStyles, handleClick }: CustomButtonProps) => {
   return (
-    <button disabled={false} type={"button"} className='custom-btn' onClick={()=>()}>
-        <span className={'flex-1'}> Title</span>
+    <button
+      disabled={false}
+      type="button"
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className="flex-1">{title}</span>
     </button>
-  )
-}
+  );
+};
 
-export default CustomButton
+export default CustomButton;

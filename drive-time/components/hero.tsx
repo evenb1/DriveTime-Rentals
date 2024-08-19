1d52f9d9e44f3e31543623ac798989fa23764245
Even <even.berhane2022@gmail.com>
2024-08-19 15:58:15 +0300

    hero section

--- drive-time/components/hero.tsx
+++ drive-time/components/hero.tsx
@@ -5,8 +5,11 @@ const hero = () => {
     <div className='hero'>
         <div className='flex-1 pt-36 padding-x'>
             <h1 className='hero__title'>
-
+            Your Journey Begins Here
             </h1>
+            <p className='hero__subtitle'>
+            Discover a wide selection of vehicles to suit every journey.
+            </p>
 
         </div>
     </div>

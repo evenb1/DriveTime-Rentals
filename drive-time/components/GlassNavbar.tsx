48dc401a4cd71ac0eb44e7dae98cd0bd3e114750
Even <even.berhane2022@gmail.com>
2024-08-23 13:19:45 +0300

    modal integration

--- drive-time/components/GlassNavbar.tsx
+++ drive-time/components/GlassNavbar.tsx
@@ -1,62 +1,58 @@
-"use client";
-
 import React, { useState } from 'react';
+import Modal from './Modal'; // Adjust the import path as necessary
 
-const GlassNavbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
+const GlassNavbar: React.FC = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
+    const toggleModal = () => {
+        setIsModalOpen(!isModalOpen);
     };
 
     return (
-        <nav className="flex justify-between items-center px-10 py-3 mx-9 my-5 bg-white bg-opacity-70 rounded-lg shadow-lg backdrop-blur-md relative">
-            {/* Middle Section: Links */}
-            <div className=" items-center font-montserrat justify-center hidden md:flex gap-5 space-x-8">
-            <a href="#" className="text-gray-600 hover:text-gray-400">Products</a>
-                <a href="#" className="text-gray-600 hover:text-gray-400">History</a>
-                <a href="#" className="text-gray-600 hover:text-gray-400">Contact</a>
-        
-            </div>
-
-            {/*Left Section: Logo */ }
-            <div className="xl:ml-[-160px] ">
-                
-            <a href="/" className="text-white text-lg  justify-center font-bold">
-                    <img src="/logonew.png" width={160} height={20} alt="Logo" />
-                </a>
-                    </div>
-
-            {/* Right Section: Sign in and Button */}
-            <div className="flex items-center space-x-4">
-                <button className="flex items-center max-sm:hidden space-x-2 bg-gray-800 font-montserrat font-light text-sm text-white py-2 px-4 rounded-full hover:bg-black">
-                Sign In
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 ml-1 h-4">
-    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-  </svg>
-  
-</button>
-            </div>
-
-            {/* Mobile Menu Toggle Button */}
-            <div className="md:hidden flex items-center">
-                <button className="text-white focus:outline-none" onClick={toggleMenu}>
-                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-                    </svg>
-                </button>
-            </div>
-
-            {/* Mobile Menu */}
-            {isOpen && (
-                <div className="absolute top-16 left-0 w-full bg-black bg-opacity-90 p-6 flex flex-col space-y-4 md:hidden">
-                    <a href="#" className="text-white hover:text-gray-300">Products</a>
-                    <a href="#" className="text-white hover:text-gray-300">History</a>
-                    <a href="#" className="text-white hover:text-gray-300">Contact</a>
-                    <a href="#" className="text-white hover:text-gray-300">Sign in</a>
+        <>
+            <nav className="flex items-center px-4 md:px-10 py-3 mx-4 md:mx-9 my-5 bg-white bg-opacity-70 rounded-lg shadow-lg backdrop-blur-md relative">
+                {/* Mobile Menu Toggle Button */}
+                <div className="md:hidden flex items-center">
+                    <button className="text-white focus:outline-none" onClick={toggleModal}>
+                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+                        </svg>
+                    </button>
                 </div>
-            )}
-        </nav>
+
+                {/* Logo Section */}
+                <div className="flex flex-grow justify-center">
+                    <a href="/" className="text-white text-lg font-bold">
+                        <img src="/logonew.png" className="w-32 md:w-40" alt="Logo" />
+                    </a>
+                </div>
+
+                {/* Navigation Links */}
+                <div className="hidden md:flex items-center gap-5 space-x-8 flex-grow justify-center">
+                    <a href="#" className="text-gray-600 hover:text-gray-400">Products</a>
+                    <a href="#" className="text-gray-600 hover:text-gray-400">History</a>
+                    <a href="#" className="text-gray-600 hover:text-gray-400">Contact</a>
+                </div>
+
+                {/* Sign In Button */}
+                <div className="flex items-center space-x-4">
+                    <button
+                        className="flex items-center space-x-2 bg-gray-800 font-montserrat font-light text-sm text-white py-2 px-4 rounded-full hover:bg-black"
+                        onClick={toggleModal}
+                    >
+                        Sign In
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="ml-1 size-5">
+  <path d="M10 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM3.465 14.493a1.23 1.23 0 0 0 .41 1.412A9.957 9.957 0 0 0 10 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 0 0-13.074.003Z" />
+</svg>
+
+
+                    </button>
+                </div>
+            </nav>
+
+            {/* Modal Component */}
+            <Modal isOpen={isModalOpen} onClose={toggleModal} />
+        </>
     );
 };
 
--- drive-time/components/Modal.tsx
+++ drive-time/components/Modal.tsx
@@ -6,7 +6,7 @@ const Modal: React.FC<{ isOpen: boolean; onClose: () => void }> = ({ isOpen, onC
     return (
         <div id="login-popup" className="bg-black/50 overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 h-full items-center justify-center flex">
             <div className="relative p-4 w-full max-w-md h-full md:h-auto">
-                <div className="relative bg-white rounded-lg shadow">
+                <div className="relative bg-white mt-14 px-4 pt-3 rounded-lg shadow">
                     <button 
                         type="button" 
                         className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
@@ -18,17 +18,15 @@ const Modal: React.FC<{ isOpen: boolean; onClose: () => void }> = ({ isOpen, onC
                         <span className="sr-only">Close popup</span>
                     </button>
                     <div className="p-5">
-                        <h3 className="text-2xl mb-0.5 font-medium">Login</h3>
-                        <p className="mb-4 text-sm font-normal text-gray-800">Please login to access your account.</p>
-                        <div className="text-center">
+                              <div className="text-center">
                             <p className="mb-3 text-2xl font-semibold leading-5 text-slate-900">Login to your account</p>
-                            <p className="mt-2 text-sm leading-4 text-slate-600">You must be logged in to perform this action.</p>
+                            <p className="mt-3 text-sm leading-4 text-slate-600">You must be logged in to perform this action.</p>
                         </div>
                         <div className="mt-7 flex flex-col gap-2">
                             {/* Social login buttons */}
                             <button className="inline-flex h-10 w-full items-center justify-center gap-2 rounded border border-slate-300 bg-white p-2 text-sm font-medium text-black outline-none focus:ring-2 focus:ring-[#333] focus:ring-offset-1">
-                                <img src="https://www.svgrepo.com/show/512317/github-142.svg" alt="GitHub" className="h-[18px] w-[18px]" />
-                                Continue with GitHub
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-[18px] w-[18px]" viewBox="0 0 128 128"><path d="M97.905 67.885c.174 18.8 16.494 25.057 16.674 25.137-.138.44-2.607 8.916-8.597 17.669-5.178 7.568-10.553 15.108-19.018 15.266-8.318.152-10.993-4.934-20.504-4.934-9.508 0-12.479 4.776-20.354 5.086-8.172.31-14.395-8.185-19.616-15.724C15.822 94.961 7.669 66.8 18.616 47.791c5.438-9.44 15.158-15.417 25.707-15.571 8.024-.153 15.598 5.398 20.503 5.398 4.902 0 14.106-6.676 23.782-5.696 4.051.169 15.421 1.636 22.722 12.324-.587.365-13.566 7.921-13.425 23.639M82.272 21.719c4.338-5.251 7.258-12.563 6.462-19.836-6.254.251-13.816 4.167-18.301 9.416-4.02 4.647-7.54 12.087-6.591 19.216 6.971.54 14.091-3.542 18.43-8.796"/></svg>
+                                Continue with Apple
                             </button>
                             <button className="inline-flex h-10 w-full items-center justify-center gap-2 rounded border border-slate-300 bg-white p-2 text-sm font-medium text-black outline-none focus:ring-2 focus:ring-[#333] focus:ring-offset-1">
                                 <img src="https://www.svgrepo.com/show/475656/google-color.svg" alt="Google" className="h-[18px] w-[18px]" />
@@ -54,10 +52,7 @@ const Modal: React.FC<{ isOpen: boolean; onClose: () => void }> = ({ isOpen, onC
                             </p>
                             <button type="submit" className="inline-flex w-full items-center justify-center rounded-lg bg-black p-2 py-3 text-sm font-medium text-white outline-none focus:ring-2 focus:ring-black focus:ring-offset-1 disabled:bg-gray-400">Continue</button>
                         </form>
-                        <div className="mt-6 text-center text-sm text-slate-600">
-                            Don't have an account?
-                            <a href="/signup" className="font-medium text-[#4285f4]">Sign up</a>
-                        </div>
+                        
                     </div>
                 </div>
             </div>

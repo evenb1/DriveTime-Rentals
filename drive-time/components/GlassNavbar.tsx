5742def29305279af7deda884235d608bb94c5ee
Even <even.berhane2022@gmail.com>
2024-08-23 12:35:33 +0300

    smaller screen size adjustment

--- drive-time/components/GlassNavbar.tsx
+++ drive-time/components/GlassNavbar.tsx
@@ -10,26 +10,26 @@ const GlassNavbar = () => {
     };
 
     return (
-        <nav className="flex justify-between items-center px-10 py-3 mx-9 my-5 bg-white bg-opacity-75 rounded-lg shadow-lg backdrop-blur-md relative">
-            {/* Left Section: Logo */}
-            <div className=" ">
+        <nav className="flex justify-between items-center px-10 py-3 mx-9 my-5 bg-white bg-opacity-70 rounded-lg shadow-lg backdrop-blur-md relative">
+            {/* Middle Section: Links */}
+            <div className=" items-center font-montserrat justify-center hidden md:flex gap-5 space-x-8">
+            <a href="#" className="text-gray-600 hover:text-gray-400">Products</a>
+                <a href="#" className="text-gray-600 hover:text-gray-400">History</a>
+                <a href="#" className="text-gray-600 hover:text-gray-400">Contact</a>
+        
+            </div>
+
+            {/*Left Section: Logo */ }
+            <div className="xl:ml-[-160px] ">
+                
             <a href="/" className="text-white text-lg  justify-center font-bold">
                     <img src="/logonew.png" width={160} height={20} alt="Logo" />
                 </a>
-            </div>
-
-            {/* Middle Section: Links */}
-            <div className=" items-center font-montserrat justify-center hidden md:flex gap-5 space-x-8">
-                
-                
-                <a href="#" className="text-gray-600 hover:text-gray-400">Products</a>
-                <a href="#" className="text-gray-600 hover:text-gray-400">History</a>
-                <a href="#" className="text-gray-600 hover:text-gray-400">Contact</a>
-            </div>
+                    </div>
 
             {/* Right Section: Sign in and Button */}
             <div className="flex items-center space-x-4">
-                <button className="flex items-center space-x-2 bg-gray-800 font-montserrat font-light text-sm text-white py-2 px-4 rounded-full hover:bg-black">
+                <button className="flex items-center max-sm:hidden space-x-2 bg-gray-800 font-montserrat font-light text-sm text-white py-2 px-4 rounded-full hover:bg-black">
                 Sign In
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 ml-1 h-4">
     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />

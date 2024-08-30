03bf3ebf0d9abd2b0e7e1f76b3ddef0355bd988b
Even <even.berhane2022@gmail.com>
2024-08-30 10:29:23 +0300

    footer logo and css properties set

--- drive-time/app/layout.tsx
+++ drive-time/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
+import { Footer } from "@/components";
 
 export const metadata: Metadata = {
   title: "DriveTime Rentals",
@@ -14,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="relative">{children}</body>
+      <body className="relative">
+        {children}
+        <Footer />
+        </body>
     </html>
   );
 }

cd6c10c65f7e5aeedaf67922d64a619a40554d67
Even <even.berhane2022@gmail.com>
2024-09-07 15:06:38 +0300

    custom filter component

--- drive-time/components/index.ts
+++ drive-time/components/index.ts
@@ -2,12 +2,13 @@ import Hero from './Hero'
 import CustomButton from './CustomButton'
 import Footer from './Footer'
 import SearchBar from './SearchBar'
+import 
 export{
     Hero,
     
     Footer,
     CustomButton,
     SearchBar,
-    
+
 
 }

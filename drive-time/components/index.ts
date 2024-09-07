ee624d38c0f535d94563304d7fab8e33e9644894
Even <even.berhane2022@gmail.com>
2024-09-07 15:06:01 +0300

    constants

--- drive-time/components/index.ts
+++ drive-time/components/index.ts
@@ -1,10 +1,13 @@
 import Hero from './Hero'
 import CustomButton from './CustomButton'
 import Footer from './Footer'
+import SearchBar from './SearchBar'
 export{
     Hero,
     
     Footer,
-    CustomButton
+    CustomButton,
+    SearchBar,
+    
 
 }

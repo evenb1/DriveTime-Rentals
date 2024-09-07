563b88d43c0bace27f1e0ca8c5be425a03b7a0ab
Even <even.berhane2022@gmail.com>
2024-09-07 15:04:41 +0300

    search bar

--- drive-time/app/page.tsx
+++ drive-time/app/page.tsx
@@ -5,12 +5,15 @@ export default function Home() {
   return (
     <main className="overflow-hidden ">
       <Hero />
+      <SearchBar/>
       <div className="mt-12 padding-x padding-y max-width" id="discover">
         <div className="home__text-container">
           <h1 className="text-4xl font-extrabold"> Our Fleet</h1>
+          <p>Explore cars you might like</p>
+        </div>
+        <div className="home__filters">
 
         </div>
-
       </div>
     </main>
   );

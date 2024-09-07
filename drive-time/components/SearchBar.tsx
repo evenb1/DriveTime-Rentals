8c37663c4abba472db8047980468df318b4d9d82
Even <even.berhane2022@gmail.com>
2024-09-07 15:18:16 +0300

    import fix

--- drive-time/components/SearchBar.tsx
+++ drive-time/components/SearchBar.tsx
@@ -1,11 +1,13 @@
 'use client';
+import SearchManufacturer from "./SearchManufacturer";
 
 const SearchBar = () => {
     const handleSearch = () => {}
   return (
     <form className='searchbar' onSubmit={handleSearch}>
         <div className="searchbar__item">
-            
+            <SearchManufacturer
+            />
         </div>
     </form>
   )

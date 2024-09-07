0aebe9854c81f24a8c64aa4904ae846152225b87
Even <even.berhane2022@gmail.com>
2024-09-07 15:10:46 +0300

    searchbar form

--- drive-time/app/page.tsx
+++ drive-time/app/page.tsx
@@ -1,4 +1,4 @@
-import { Hero } from "@/components";
+import { Hero, SearchBar, CustomFilter } from "@/components";
 import Image from "next/image";
 import GlassNavbar from "@/components/GlassNavbar";
 export default function Home() {
@@ -6,6 +6,7 @@ export default function Home() {
     <main className="overflow-hidden ">
       <Hero />
       <SearchBar/>
+      <CustomFilter/>
       <div className="mt-12 padding-x padding-y max-width" id="discover">
         <div className="home__text-container">
           <h1 className="text-4xl font-extrabold"> Our Fleet</h1>
--- drive-time/components/SearchBar.tsx
+++ drive-time/components/SearchBar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
+    <form className='searchbar'></form>
   )
 }
 

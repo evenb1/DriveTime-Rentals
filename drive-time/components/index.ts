8d23be5bff8cfea1d71ea32f4719b3567f741bc1
Even <even.berhane2022@gmail.com>
2024-09-07 15:07:20 +0300

    constants (custom filter)

--- drive-time/components/index.ts
+++ drive-time/components/index.ts
@@ -2,13 +2,13 @@ import Hero from './Hero'
 import CustomButton from './CustomButton'
 import Footer from './Footer'
 import SearchBar from './SearchBar'
-import 
+import CustomFilter from './CustomFilter'
 export{
     Hero,
     
     Footer,
     CustomButton,
     SearchBar,
-
+    CustomFilter
 
 }

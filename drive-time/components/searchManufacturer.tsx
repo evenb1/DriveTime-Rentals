93ed971eea8d36d0b8992555e70f8308494dabea
Even <even.berhane2022@gmail.com>
2024-09-16 14:56:16 +0300

    combobox button

--- drive-time/components/searchManufacturer.tsx
+++ drive-time/components/searchManufacturer.tsx
@@ -1,16 +1,23 @@
-import { SearchManufacturerProps } from '@/types'
-import { Combobox, Transition } from '@headlessui/react'
+"use client";
 
-const searchManufacturer = ({manufacturer, setManufacturer} : SearchManufacturerProps) => {
+import Image from 'next/image';
+import { SearchManufacturerProps } from '@/types';
+import { Combobox } from '@headlessui/react';
+
+const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacturerProps) => {
   return (
     <div className='search-manufacturer'>
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className="relative w-full">
-          
+          <Combobox.Button className="absolute top-[14px]">
+            <Image src="/car-logo.svg" width={20} height={20} className="ml-4" alt="Car Logo" />
+          </Combobox.Button>
+
+          {/* Add Combobox.Input if needed */}
         </div>
       </Combobox>
     </div>
-  )
-}
+  );
+};
 
-export default searchManufacturer
+export default SearchManufacturer;

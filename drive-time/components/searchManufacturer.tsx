f900a8af49296ab2a78c529679dc4fe624dc7ba9
Even <even.berhane2022@gmail.com>
2024-09-16 15:04:55 +0300

    new usestate snippet

--- drive-time/components/searchManufacturer.tsx
+++ drive-time/components/searchManufacturer.tsx
@@ -1,10 +1,11 @@
 "use client";
-
+import { useState } from 'react';
 import Image from 'next/image';
 import { SearchManufacturerProps } from '@/types';
 import { Combobox } from '@headlessui/react';
 
 const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacturerProps) => {
+  const [query, setquery] = useState('')
   return (
     <div className='search-manufacturer'>
       <Combobox value={manufacturer} onChange={setManufacturer}>
@@ -12,8 +13,9 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
           <Combobox.Button className="absolute top-[14px]">
             <Image src="/car-logo.svg" width={20} height={20} className="ml-4" alt="Car Logo" />
           </Combobox.Button>
-          <Combobox.Input>
-            
+          <Combobox.Input className="search-manufacturer__input" placeholder='Volkswagen'
+          displayValue={(manufacturer: string)=>manufacturer} onChange={(e) => setquery(e.target.value)}>
+
           </Combobox.Input>
         </div>
       </Combobox>

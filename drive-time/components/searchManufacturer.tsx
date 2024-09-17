60a86094b077391b5d5294b57174d1c121b3c622
Even <even.berhane2022@gmail.com>
2024-09-17 14:34:00 +0300

    combobox transition

--- drive-time/components/searchManufacturer.tsx
+++ drive-time/components/searchManufacturer.tsx
@@ -1,11 +1,15 @@
 "use client";
-import { useState } from 'react';
+import { useState, Fragment } from 'react';
 import Image from 'next/image';
 import { SearchManufacturerProps } from '@/types';
-import { Combobox } from '@headlessui/react';
+import { Combobox, ComboboxOptions, Transition } from '@headlessui/react';
+import { manufacturers } from '@/constants';
 
 const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacturerProps) => {
-  const [query, setquery] = useState('')
+  const [query, setquery] = useState('');
+  const filtereManufacturers= query === "" ? manufacturers : 
+  manufacturers.filter((item) => item.toLowerCase().replace(/\s+/g, "").includes(query.toLowerCase().replace(/\s+/g, "")))
+
   return (
     <div className='search-manufacturer'>
       <Combobox value={manufacturer} onChange={setManufacturer}>
@@ -14,9 +18,14 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
             <Image src="/car-logo.svg" width={20} height={20} className="ml-4" alt="Car Logo" />
           </Combobox.Button>
           <Combobox.Input className="search-manufacturer__input" placeholder='Volkswagen'
-          displayValue={(manufacturer: string)=>manufacturer} onChange={(e) => setquery(e.target.value)}>
+          displayValue={(manufacturer: string)=>manufacturer} onChange={(e) => setquery(e.target.value)}/>
+          <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom='opactiy-100' afterLeave={() => setquery('')}>
+            <ComboboxOptions >
 
-          </Combobox.Input>
+            </ComboboxOptions>
+
+          </Transition>
+
         </div>
       </Combobox>
     </div>

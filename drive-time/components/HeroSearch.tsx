fae3f1e40bba8a8def5da8acc1744f9f1780152a
Even <even.berhane2022@gmail.com>
2024-10-28 15:06:24 +0300

    container for search

---
+++ drive-time/components/HeroSearch.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroSearch = () => {
+  return (
+    <div className='flex justify-between items-center '>
+        
+        
+    </div>
+  )
+}
+
+export default HeroSearch

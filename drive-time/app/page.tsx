bffaa9773406069608c915769a9370735bd3e13c
Even <even.berhane2022@gmail.com>
2024-10-30 19:40:25 +0300

    home page error handling

--- drive-time/app/page.tsx
+++ drive-time/app/page.tsx
@@ -2,10 +2,15 @@ import { Hero, SearchBar, CustomFilter } from "@/components";
 import Image from "next/image";
 import GlassNavbar from "@/components/GlassNavbar";
 import { fetchCars } from "@/utils";
+import { div } from "framer-motion/client";
 
 
 export default async function Home() {
+
   const allcars = await fetchCars();
+  const isDataEmpty = !Array.isArray(allcars) || allcars.length < 1 || !allcars;
+
+  console.log(allcars)
   return (
     <main className="overflow-hidden ">
       <Hero />
@@ -23,6 +28,18 @@ export default async function Home() {
           <CustomFilter title="year"/>
         </div>
       </div>
+
+      {!isDataEmpty ? (
+        <section>
+            We have cars
+        </section>
+      ) : (
+        <div className="home__error-container">
+          <h2 className="text-black font-bold">Oops, no Reuslts</h2>
+          <p>{allcars?.message}</p>
+        </div>
+        
+      )}
     </main>
   );
 }

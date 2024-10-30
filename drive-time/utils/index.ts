7be0623d58bdfffd148d0e65ac47259f132431cb
Even <even.berhane2022@gmail.com>
2024-10-30 17:34:24 +0300

    fetch cars function

--- drive-time/utils/index.ts
+++ drive-time/utils/index.ts
@@ -19,4 +19,10 @@ export async function fetchCars(){
         'x-rapidapi-key': '6f8b752166mshd72cbd3229f278ap139b64jsna99ee7e78f37',
     'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
+    const response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
+        headers: headers,
+    });
+
+    const result = await response.json();
+    return result;
 }

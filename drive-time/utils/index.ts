c01358dc08e57c0316554febbea22eb6de685d9b
Even <even.berhane2022@gmail.com>
2024-10-30 17:25:48 +0300

    axios to fetch change

--- drive-time/utils/index.ts
+++ drive-time/utils/index.ts
@@ -1,9 +1,8 @@
-const axios = require('axios');
+const fetch = require('node-fetch');
 
+const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
 const options = {
   method: 'GET',
-  url: 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
-  params: {model: 'corolla'},
   headers: {
     'x-rapidapi-key': '6f8b752166mshd72cbd3229f278ap139b64jsna99ee7e78f37',
     'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
@@ -11,8 +10,9 @@ const options = {
 };
 
 try {
-	const response = await axios.request(options);
-	console.log(response.data);
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
 } catch (error) {
 	console.error(error);
 }

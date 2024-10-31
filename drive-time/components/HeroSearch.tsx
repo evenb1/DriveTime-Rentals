8a13ffc83bbe673cb5e765480f1e3d15691e8756
Even <even.berhane2022@gmail.com>
2024-10-31 19:43:03 +0300

    location input

--- drive-time/components/HeroSearch.tsx
+++ drive-time/components/HeroSearch.tsx
@@ -7,7 +7,16 @@ const HeroSearch = () => {
 
   return (
     <div className='flex items-center justify-between bg-white shadow-lg rounded-lg p-4 max-w-4xl mx-auto'>
-        
+        <div className="flex flex-col items-start">
+                <label className="text-gray-800 font-medium mb-1">Location</label>
+                <input
+                    type="text"
+                    placeholder="Search your location"
+                    value={location}
+                    onChange={(e) => setLocation(e.target.value)}
+                    className="border border-gray-300 rounded-lg px-4 py-2 w-full text-gray-600 focus:outline-none focus:border-blue-500"
+                />
+            </div>
         
     </div>
   )

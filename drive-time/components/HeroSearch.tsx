2a0a0e3abb8d1a417b071fb2c208dca0b587fa66
Even <even.berhane2022@gmail.com>
2024-10-31 19:41:34 +0300

    return date const

--- drive-time/components/HeroSearch.tsx
+++ drive-time/components/HeroSearch.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 const HeroSearch = () => {
   const [location, setLocation] = useState('');
   const [pickupDate, setPickupDate] = useState('');
+  const [returnDate, setReturnDate] = useState('');
 
   return (
     <div className='flex justify-between items-center '>

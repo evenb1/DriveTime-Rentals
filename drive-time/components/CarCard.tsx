092e4c619dc2e2e8da49cafac1a31ccab55df99c
Even <even.berhane2022@gmail.com>
2024-11-03 22:21:19 +0300

    card styling

--- drive-time/components/CarCard.tsx
+++ drive-time/components/CarCard.tsx
@@ -10,7 +10,20 @@ interface CarCardProps{
 const CarCard = ({car}: CarCardProps) => {
      const {city_mpg, year, make, model, transmission, drive} = car;
   return (
-    <div>CarCard</div>
+    <div className="car-card group">
+        <div className="car-card__content">
+            <h2 className="car-card__content-title">
+                {make} {model}
+            </h2>
+
+        </div>
+        <p className="flex mt-6 text-[32px] font-extrabold">
+            <span className="self-start text-[14px] font-semibold">$</span>
+            100
+            <span className="self-end text-[14px] font-medium">/day</span>
+
+        </p>
+    </div>
   )
 }
 

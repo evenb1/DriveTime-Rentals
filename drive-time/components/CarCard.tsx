0a038fca277abc7a2cd529d1133631f62bd82980
Even <even.berhane2022@gmail.com>
2024-11-04 10:43:26 +0300

    butten render issue

--- drive-time/components/CarCard.tsx
+++ drive-time/components/CarCard.tsx
@@ -9,7 +9,8 @@ interface CarCardProps{
 }
 const CarCard = ({car}: CarCardProps) => {
      const {city_mpg, year, make, model, transmission, drive} = car;
-     const [IsOpen, setIsOpen] = useState(false)
+     const [IsOpen, setIsOpen] = useState(false);
+
   return (
     <div className="car-card group">
         <div className="car-card__content">
@@ -25,7 +26,7 @@ const CarCard = ({car}: CarCardProps) => {
 
         </p>
         <div className="relative w-full h-40 my-3 object-contain">
-            <Image src="/" alt="car model" fill priority className="object-contain"/>
+            <Image src="/car-logo.svg" alt="car model" fill priority className="object-contain"/>
 
         </div>
         <div className="relative flex w-full mt-2">
@@ -50,7 +51,7 @@ const CarCard = ({car}: CarCardProps) => {
                 </div>
 
             </div>
-            <div className="card-card__btn-container">
+            <div className="car-card__btn-container">
                 <CustomButton title="View More" containerStyles="w-full py-[16px] rounded-full bg-primary-blue" textStyles="text-white text-[14px] leading-[17px] font-bold"
                 rightIcon="/right-arrow.svg"
                 handleClick={() => setIsOpen(true)}

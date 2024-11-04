5b713fc4016d5d69ddeb8d79b4138be5f2f12bc4
Even <even.berhane2022@gmail.com>
2024-11-04 10:31:58 +0300

    custom button

--- drive-time/components/CustomButton.tsx
+++ drive-time/components/CustomButton.tsx
@@ -1,15 +1,20 @@
 "use client";
 import { CustomButtonProps } from '@/types';
-
-const CustomButton = ({ title, containerStyles, handleClick }: CustomButtonProps) => {
+import Image from 'next/image';
+const CustomButton = ({ title, containerStyles, handleClick, textStyles, rightIcon,btnType }: CustomButtonProps) => {
   return (
     <button
       disabled={false}
-      type="button"
+      type={btnType || "button"}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className="flex-1">{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className='relative w-6 h-6'>
+          <Image src={rightIcon} alt="right icon" fill className='object-contain'/>
+        </div>
+      )}
     </button>
   );
 };

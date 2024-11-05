d6ddb7033e8a9f0b095d4e4f734e21652f7af765
Even <even.berhane2022@gmail.com>
2024-11-05 20:31:30 +0300

    transition dialog

--- drive-time/components/CarDetails.tsx
+++ drive-time/components/CarDetails.tsx
@@ -14,7 +14,9 @@ const CarDetails = ({isOpen, closeModal, car}:CarsDetailsProps) => {
   return (
     <>
     <Transition appear show={isOpen} as={Fragment}>
-      
+      <dialog as="div" className='relative z-10' onclose={closeModal}>
+        
+      </dialog>
     </Transition>
     </>
   )

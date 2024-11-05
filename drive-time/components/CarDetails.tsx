9bc1f30c1e75dc9971d7f06ddd9aeec1eb6db030
Even <even.berhane2022@gmail.com>
2024-11-05 20:39:05 +0300

    dialog modal

--- drive-time/components/CarDetails.tsx
+++ drive-time/components/CarDetails.tsx
@@ -21,6 +21,11 @@ const CarDetails = ({isOpen, closeModal, car}:CarsDetailsProps) => {
         >
           <div className='fixed inset-0 bg-black bg-opacity-25'/>
         </Transition.Child>
+        <div className="fixed inset-0 overflow-y-auto">
+        <div className="flex min-h-full items-center justify-center p-4 text-center">
+          
+        </div>
+        </div>
       </Dialog>
     </Transition>
     </>

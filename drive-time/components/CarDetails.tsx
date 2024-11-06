c87a6e89aa2cbe19c6a482b207fa05ded749f02a
Even <even.berhane2022@gmail.com>
2024-11-06 09:54:19 +0300

    panel styling

--- drive-time/components/CarDetails.tsx
+++ drive-time/components/CarDetails.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { carProps } from '@/types';
 import React from 'react'
+import Image from "next/image";
 import { Fragment } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 
@@ -26,14 +27,28 @@ const CarDetails = ({isOpen, closeModal, car}:CarsDetailsProps) => {
         <Transition.Child 
         as={Fragment} 
         enter="ease-out duration-300" 
-        enterFrom='opacity-o scale-95' 
+        enterFrom='opacity-0 scale-95' 
         enterTo='opacity-100 scale-100' 
         leave='ease-in duration-200'
         leaveFrom='opactiy-100 scale-100' 
         leaveTo='opactiy-0 scale-100'
         >
-          <Dialog.Panel>
-            
+          <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white
+          text-left shadow-xsl transition-all flex flex-col flex-5 gap-5 ">
+          <button
+                  type='button'
+                  className='absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full'
+                  onClick={closeModal}
+                >
+                  <Image
+                    src='/close.svg'
+                    alt='close'
+                    width={20}
+                    height={20}
+                    className='object-contain'
+                  />
+                </button>
+
           </Dialog.Panel>
         </Transition.Child>
         </div>

b6c3752ee499a435df2b123d6336b3b7931f9c30
Even <even.berhane2022@gmail.com>
2024-11-10 12:00:17 +0300

    page structure

--- drive-time/components/dashboard/page.tsx
+++ drive-time/components/dashboard/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import DashboardWidgets from "./components/DashboardWidgets";
+import RecentActivity from "./components/RecentActivity";
 
-const page = () => {
+export default function DashboardPage() {
   return (
-    <div>page</div>
-  )
+    <div className="dashboard-overview">
+      <h1 className="dashboard-title">Welcome to Drivetime Rental Dashboard</h1>
+      <DashboardWidgets />
+      <RecentActivity />
+    </div>
+  );
 }
-
-export default page

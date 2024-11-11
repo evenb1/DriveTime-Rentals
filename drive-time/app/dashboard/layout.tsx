0ff479d98c0b806b68e6b11994e8d8d213430586
Even <even.berhane2022@gmail.com>
2024-11-11 10:19:29 +0300

    dash layout

--- drive-time/app/dashboard/layout.tsx
+++ drive-time/app/dashboard/layout.tsx
@@ -1,19 +1,11 @@
-import React from "react";
-import TopNav from "./components/TopNav";
-import SideBar from "./components/Sidebar";
+import React from 'react';
+import Sidebar from './components/Sidebar';
 
-export default function DashboardLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen">
-      <SideBar/>
-      <div className="flex-1 flex flex-col">
-        <TopNav />
-        <main className="p-6 bg-gray-100 flex-1">{children}</main>
-      </div>
+    <div className="flex">
+      <Sidebar />
+      <div className="flex-1">{children}</div>
     </div>
   );
 }

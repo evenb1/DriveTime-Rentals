89e54b52bd5ce82105ba5ef3d6ca5055ee9c1e06
Even <even.berhane2022@gmail.com>
2024-11-13 15:11:12 +0300

    banner adjustments

--- drive-time/app/dashboard/page.tsx
+++ drive-time/app/dashboard/page.tsx
@@ -12,7 +12,7 @@ const HomeDashboard = () => {
         
         {/* Welcome Banner */}
         <motion.div
-          className="bg-gradient-to-r from-blue-500 to-indigo-600 rounded-lg p-6 shadow-lg text-white"
+          className="bg-gradient-to-r from-blue-500 to-indigo-600 rounded-lg p-6  text-white"
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}

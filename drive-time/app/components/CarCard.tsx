fc38810734f2878b08af49680137224020b25ddf
Even <even.berhane2022@gmail.com>
2024-11-15 20:35:48 +0300

    view more functionality

--- drive-time/app/components/CarCard.tsx
+++ drive-time/app/components/CarCard.tsx
@@ -69,3 +69,14 @@ const CarCard = ({ car }: CarCardProps) => {
             <p className="text-[14px]">{city_mpg}</p>
           </div>
         </div>
+{/* View More Button */}
+<div className="car-card__btn-container">
+          <CustomButton
+            title="View More"
+            containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick={() => setIsOpen(true)}
+          />
+        </div>
+      </div>

5aa2ede822921eb204d5fee86a4665e80f7fee6e
Even <even.berhane2022@gmail.com>
2024-11-15 20:35:02 +0300

    Main Car Image render

--- drive-time/app/components/CarCard.tsx
+++ drive-time/app/components/CarCard.tsx
@@ -26,4 +26,26 @@ const CarCard = ({ car }: CarCardProps) => {
   
     // Get the image for the car's make from the carImages object
     const mainImage = carImages[make.toLowerCase()] || "/default-placeholder.png"; // Fallback to a placeholder if not found
-  
+    return (
+        <div className="car-card group">
+          <div className="car-card__content">
+            <h2 className="car-card__content-title">
+              {make} {model}
+            </h2>
+          </div>
+          <p className="flex mt-6 text-[32px] font-extrabold">
+            <span className="self-start text-[14px] font-semibold">$</span>
+            100
+            <span className="self-end text-[14px] font-medium">/day</span>
+          </p>
+          {/* Main Car Image */}
+      <div className="relative w-full h-40 my-3 object-contain">
+        <Image
+          src={mainImage}
+          alt={`${make} ${model}`}
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" // Responsive sizes for better performance
+          priority
+          className="object-contain"
+        />
+      </div>

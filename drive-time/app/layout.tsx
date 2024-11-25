31e1f13b2fe3f7c80e1766af9cd372bebd08d06e
Even <even.berhane2022@gmail.com>
2024-11-25 21:48:07 +0300

    session wrapper file

--- drive-time/app/layout.tsx
+++ drive-time/app/layout.tsx
@@ -1,26 +1,23 @@
-import { SessionProvider } from "next-auth/react"; // Import SessionProvider
 import { Footer } from "@/app/components"; // Import Footer
-import { Metadata } from "next"; // Import Metadata (for server-side use)
+import { Metadata } from "next"; // Import Metadata
 
 export const metadata: Metadata = {
   title: "DriveTime Rentals",
   description: "Discover the best cars in the world",
 };
 
-// Remove "use client" here because metadata should be defined in server-side layout
+// Keep this layout as a server-side component
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <SessionProvider>
-      <html lang="en">
-        <body className="relative">
-          {children} {/* Render children (pages, dashboard) */}
-          <Footer />
-        </body>
-      </html>
-    </SessionProvider>
+    <html lang="en">
+      <body className="relative">
+        {children} {/* Render children (pages, dashboard) */}
+        <Footer />
+      </body>
+    </html>
   );
 }

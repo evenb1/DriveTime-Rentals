ad420ef466c1a2c158628b05e0d691c5361e06f6
Even <even.berhane2022@gmail.com>
2024-11-30 00:05:32 +0300

    email section

--- drive-time/app/components/Contact.tsx
+++ drive-time/app/components/Contact.tsx
@@ -33,7 +33,26 @@ const Contact = () => {
           </motion.div>
           
           {/* Email Section */}
+          <motion.div
+            className="flex flex-row items-center gap-3"
           
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-8 h-8 text-slate-100"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+              />
+            </svg>
+            <p className="text-slate-100 font-extralight text-xl">customersupport@drivetime.com</p>
+          </motion.div>
         </div>
       );
 }

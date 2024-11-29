888effd2651f7905794fbfb5062340843f5e8ebc
Even <even.berhane2022@gmail.com>
2024-11-30 00:06:04 +0300

    logos component

---
+++ drive-time/app/components/Logos.tsx
@@ -0,0 +1,20 @@
+"use client"
+import React from 'react'
+import Image from 'next/image'
+import porsche from '@/public/porche.svg'
+import lexus from '@/public/lexy.svg'
+import mercedes from '@/public/mercedes.svg'
+import caddy from '@/public/caddy.svg'
+import rover from '@/public/rover.svg'
+import { motion } from 'framer-motion'
+
+
+const Logos = () => {
+  return (
+    <motion.div className='flex flex-row gap-14 items-center justify-center '
+    
+    </motion.div>
+  )
+}
+
+export default Logos

dce11708a57747fe2b65ce6822b0137cd668966c
Even <even.berhane2022@gmail.com>
2024-12-01 13:03:37 +0300

    search manufacturer

--- drive-time/app/components/SearchManufacturer.tsx
+++ drive-time/app/components/SearchManufacturer.tsx
@@ -17,7 +17,7 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
   return (
     <div className="search-manufacturer">
       <Combobox value={manufacturer} onChange={setManufacturer}>
-        <div className="relative w-full">
+        <div className="relative w-44 rounded-lg">
           <Combobox.Button className="absolute top-[14px]">
             <Image src="/car-logo.svg" width={20} height={20} className="ml-4" alt="Car Logo" />
           </Combobox.Button>

d043b4e9990d33f3f31b3e0321ec5e9ad8e9c07d
Even <even.berhane2022@gmail.com>
2025-01-27 18:41:56 +0300

    features two responsiveness

--- drive-time/app/components/FeaturesTwo.tsx
+++ drive-time/app/components/FeaturesTwo.tsx
@@ -1,45 +1,48 @@
-"use client"
+"use client";
 import Image from "next/image";
 import driver from "@/public/driver.jpg";
 import { motion } from "framer-motion";
 
 const FeaturesTwo = () => {
   return (
-    <section className="p-0 flex flex-row my-8 bg-slate-100">
-
+    <section className="p-6 flex flex-col-reverse lg:flex-row items-center my-8 bg-slate-100 gap-8">
       {/* Text Section */}
-      <motion.div 
-     initial={{ opacity: 0, y: 60 }}
-     whileInView={{ opacity: 1, y: 0 }}
-     transition={{ duration: 1 }}
-     viewport={{ once: true, amount: 0.4 }}
-
-      className="flex flex-col items-start p-10 m-auto lg:p-28 pr-0 ">
-        <h1 className="text-slate-900 mb-1 font-light font-montserrat text-4xl lg:text-5xl">
+      <motion.div
+        initial={{ opacity: 0, y: 60 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        viewport={{ once: true, amount: 0.4 }}
+        className="flex flex-col items-center lg:items-start p-6 lg:p-16"
+      >
+        <h1 className="text-slate-900 mb-4 font-light font-montserrat text-3xl lg:text-5xl text-center lg:text-left">
           We Care About Your <br />
           Comfort And Safety
         </h1>
-        <h3 className="text-blue-400 mb-2 font-inter font-light text-lg">
+        <h3 className="text-blue-400 mb-4 font-inter font-light text-lg text-center lg:text-left">
           Experienced Drivers
         </h3>
-        <p className="text-slate-900 text-lg  w-2/3 lg:text-xl font-extralight">
-        We prioritize your peace of mind with experienced drivers and top-tier vehicles, delivering a smooth, secure, and enjoyable travel experience.
+        <p className="text-slate-900 text-lg lg:text-xl font-extralight text-center lg:text-left max-w-lg">
+          We prioritize your peace of mind with experienced drivers and
+          top-tier vehicles, delivering a smooth, secure, and enjoyable travel
+          experience.
         </p>
       </motion.div>
 
       {/* Image Section */}
-      <div className=" flex px-0"
-      
-      >
-        <motion.div className="relative w-full items-end justify-end"
+      <motion.div
+        className="relative w-full lg:w-1/2"
         initial={{ opacity: 0, x: -30 }}
         whileInView={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.9 }}
         viewport={{ once: true, amount: 0.4 }}
-        >
-          <Image src={driver} className="object-contain" alt="chauffer" />
-        </motion.div>
-      </div>
+      >
+        <Image
+          src={driver}
+          className="object-contain rounded-lg"
+          alt="chauffeur"
+          priority
+        />
+      </motion.div>
     </section>
   );
 };

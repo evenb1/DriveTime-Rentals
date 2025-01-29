cdbfd886d917e1a2645d79f9e3edcbb8a828b172
Even <even.berhane2022@gmail.com>
2025-01-29 13:27:15 +0300

    layout file auth provider wrap

--- drive-time/app/layout.tsx
+++ drive-time/app/layout.tsx
@@ -1,29 +1,23 @@
 import { Metadata } from "next";
-import { SessionProvider } from "next-auth/react"; 
-import { Footer } from "@/app/components"; 
-import "./globals.css"; 
-import SessionWrapper from "./SessionWrapper";
+import { Footer } from "@/app/components";
+import "./globals.css";
 import { ToastContainer } from "react-toastify";
+import { AuthProvider } from "@/context/AuthContext"; // Use Supabase Auth
 
 export const metadata: Metadata = {
   title: "DriveTime Rentals",
   description: "Discover the best cars in the world",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="relative">
-        <SessionWrapper>
-        <ToastContainer />
-
-          {children} 
+        <AuthProvider> {/* Wrap with AuthProvider */}
+          <ToastContainer />
+          {children}
           <Footer />
-        </SessionWrapper>
+        </AuthProvider>
       </body>
     </html>
   );

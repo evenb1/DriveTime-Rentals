318381ab756e8f6c9e940478129260613c78e940
Even <even.berhane2022@gmail.com>
2025-01-29 16:12:03 +0300

    final

--- drive-time/app/components/Logos.tsx
+++ drive-time/app/components/Logos.tsx
@@ -10,9 +10,9 @@ import rover from "@/public/rover.svg";
 
 const Logos = () => {
   return (
-    <div className="overflow-hidden py-6">
+    <div className="relative w-full overflow-hidden py-6">
       <motion.div
-        className="flex gap-10 items-center whitespace-nowrap"
+        className="flex gap-10 items-center w-full h-20 whitespace-nowrap"
         animate={{ x: ["0%", "-100%"] }}
         transition={{
           duration: 35, // Slower scrolling speed
@@ -24,13 +24,13 @@ const Logos = () => {
         {[...Array(2)].map((_, index) => (
           <React.Fragment key={index}>
             <Image src={lexus} alt="Lexus" className="w-20 sm:w-28" />
-            <span className="hidden md:inline-block w-px h-24 bg-gray-300" />
+            <span className="hidden md:inline-block w-px h-16 bg-gray-300" />
             <Image src={porsche} alt="Porsche" className="w-20 sm:w-28" />
-            <span className="hidden md:inline-block w-px h-24 bg-gray-300" />
+            <span className="hidden md:inline-block w-px h-16 bg-gray-300" />
             <Image src={mercedes} alt="Mercedes" className="w-20 sm:w-28" />
-            <span className="hidden md:inline-block w-px h-24 bg-gray-300" />
+            <span className="hidden md:inline-block w-px h-16 bg-gray-300" />
             <Image src={rover} alt="Range Rover" className="w-20 sm:w-28" />
-            <span className="hidden md:inline-block w-px h-24 bg-gray-300" />
+            <span className="hidden md:inline-block w-px h-16 bg-gray-300" />
             <Image src={caddy} alt="Cadillac" className="w-20 sm:w-28 mr-7" />
           </React.Fragment>
         ))}
